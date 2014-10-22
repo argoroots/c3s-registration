c565b6b7e9056eca03cd22a4da7c907798b13611
Argo Roots <argo@Argo-MBP.local>
2014-10-22 10:25:52 +0300

    Correct names for form field; Hide/show fields based on role;

---
+++ source/javascripts/script.js
@@ -0,0 +1,29 @@
+function cl(d) {
+    console.log(d)
+}
+
+var API_URL  = PAGE_URL + 'https://hitsa.entu.ee/api/'
+
+angular.module('s3cApp', ['ngRoute', 'ngResource'])
+
+
+
+// ROUTER
+    .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+        // $locationProvider.html5Mode(true)
+        $routeProvider
+            .when('/', {
+                templateUrl: 'start',
+                controller: 'startCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            })
+    }])
+
+
+
+// START
+    .controller('startCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+
+    }])
