1bd325add7e07b0aaa33f69fb371eb95ac3bcaec
Argo Roots <argo@Argo-MBP.local>
2014-10-22 17:44:43 +0300

    Create application and send email

--- gruntfile.js
+++ gruntfile.js
@@ -66,7 +66,9 @@ module.exports = function (grunt) {
                         'bower_components/angular/angular.js',
                         'bower_components/angular-route/angular-route.js',
                         'bower_components/angular-resource/angular-resource.js',
-                        'bower_components/angular-sanitize/angular-sanitize.js'
+                        'bower_components/angular-sanitize/angular-sanitize.js',
+                        'bower_components/crypto-js/rollups/aes.js',
+                        'bower_components/crypto-js/rollups/hmac-sha1.js'
                     ]
                 }
             },
--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -2,7 +2,45 @@ function cl(d) {
     console.log(d)
 }
 
-var API_URL  = 'https://hitsa.entu.ee/api/'
+
+
+function getSignedData(user, key, data) {
+    if(!user || !key) return
+
+    var conditions = []
+    for(k in data) {
+        conditions.push({k: data[k]})
+    }
+
+    var expiration = new Date()
+    expiration.setMinutes(expiration.getMinutes() + 10)
+
+    data['user'] = user
+    data['policy'] = CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(JSON.stringify({expiration: expiration.toISOString(), conditions: conditions})))
+    data['signature'] = CryptoJS.enc.Base64.stringify(CryptoJS.HmacSHA1(data['policy'], key))
+
+    return data
+}
+
+
+
+function makeKey() {
+    var text = ''
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for(var i=0; i < 64; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return text
+}
+
+
+
+var PAGE_URL   = 'http://hitsa.github.io/c3s-registration/'
+var API_URL    = 'https://dev.entu.ee/api2/'
+var API_FOLDER = 619
+var API_USER   = 621
+var API_KEY    = '2jBg6SXqqxd3Z8Qas3fAM47wyDC4W6aJ'
 
 angular.module('s3cApp', ['ngRoute', 'ngResource'])
 
@@ -16,7 +54,7 @@ angular.module('s3cApp', ['ngRoute', 'ngResource'])
                 templateUrl: 'start',
                 controller: 'startCtrl'
             })
-            .when('/:id', {
+            .when('/:application_id/:key', {
                 templateUrl: 'application',
                 controller: 'applicationCtrl'
             })
@@ -30,8 +68,56 @@ angular.module('s3cApp', ['ngRoute', 'ngResource'])
 // START
     .controller('startCtrl', ['$scope', '$http', function($scope, $http) {
 
+        $scope.key = makeKey()
+
         $scope.sendLink = function() {
             $scope.sending = true
+            $http({
+                    method : 'POST',
+                    url    : API_URL + 'entity-' + API_FOLDER,
+                    data   : getSignedData(API_USER, API_KEY, {
+                        'definition': 'c3sregistration',
+                        'c3sregistration-email': $scope.email,
+                        'c3sregistration-entu-api-key': $scope.key
+                    })
+                })
+                .success(function(data) {
+                    $scope.id = data.result.id
+                    $http({
+                            method : 'POST',
+                            url    : API_URL + 'entity-' + $scope.id + '/rights',
+                            data   : getSignedData(API_USER, API_KEY, {
+                                'entity': $scope.id,
+                                'right': 'editor'
+                            })
+                        })
+                        .success(function(data) {
+                            var url = PAGE_URL + '#/' + $scope.id + '/' + $scope.key
+                            $http({
+                                    method : 'POST',
+                                    url    : API_URL + 'email',
+                                    data   : getSignedData(API_USER, API_KEY, {
+                                        'to': $scope.email,
+                                        'subject': '2015 Cyber Security Summer School',
+                                        'message': 'Here is the link to your personal application form of 2015 Cyber Security Summer School:\n\n' + url + '\n\nDo not share it!'
+                                    })
+                                })
+                                .success(function(data) {
+                                    $scope.sending = false
+                                    $scope.sent = true
+                                })
+                                .error(function(data) {
+                                    cl(data)
+                                    $scope.sending = false
+                                })
+                        })
+                        .error(function(data) {
+                            $scope.sending = false
+                        })
+                })
+                .error(function(data) {
+                    $scope.sending = false
+                })
         }
 
     }])
@@ -39,7 +125,26 @@ angular.module('s3cApp', ['ngRoute', 'ngResource'])
 
 
 // APPLICATION
-    .controller('applicationCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    .controller('applicationCtrl', ['$scope', '$http', '$routeParams', '$location', function($scope, $http, $routeParams, $location) {
+        $http({
+                method : 'GET',
+                url    : API_URL + 'entity-' + $routeParams.application_id,
+                params : getSignedData($routeParams.application_id, $routeParams.key, {})
+            })
+            .success(function(data) {
+                cl(data)
+                // $scope.request_count += 1
+                // try        { $scope.title = data.result.displayname }
+                // catch(err) { $scope.title = '' }
+                // try        { $scope.description = data.result.properties.pollheader.values[0].value }
+                // catch(err) { $scope.description = '' }
+                // try        { $scope.howto = data.result.properties.pollhowto.values[0].value }
+                // catch(err) { $scope.howto = '' }
+            })
+            .error(function(data) {
+                cl(data)
+                // $location.path('/')
+            })
 
 
     }])
